663142110449e3b35e13013e1e0505fd6982f151
Oguzhnblt <oguzhanbolat0@gmail.com>
2023-08-13 12:08:32 +0300

    Initial Commit

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Oguzhan Bolat on 01/07/2023.
+//  Copyright © 2023 Oguzhan Bolat. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
