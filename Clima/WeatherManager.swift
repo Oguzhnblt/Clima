af358bf4c48deb149b2e6e4ea1492fa093689c8c
Oguzhnblt <oguzhanbolat0@gmail.com>
2023-08-13 14:35:17 +0300

    Added WeatherModel struct

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -9,18 +9,48 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=305fe031ca7a3efd29754c845099e1de&units=metric"
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=305fe031ca7a3efd29754c845099e1de&units=metric"
     
     func fetchWeather(cityName: String){
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
     
     func performRequest(urlString: String){
         
         // 1. Create a URL
-        // 2. Create a URLSession
-        // 3. Give the session a task
-        // 4. Start the task 
+        if let url = URL(string: urlString) {
+            
+            // 2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give the session a task
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                    
+                }
+            }
+            
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON (weatherData: Data) {
+        let decoder = JSONDecoder()
+        do{
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.main.temp)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
     }
 }
